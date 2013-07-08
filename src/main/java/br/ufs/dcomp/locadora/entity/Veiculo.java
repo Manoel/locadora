dd7894b38402fb2b6079f37c78d6e111fcc253f2
Manoel Menezes <manoel.menezes.jr@gmail.com>
2013-07-08 14:18:22 -0300

    incluindo dependencias no pom.xml para os frameworks utilizados no projeto: spring, primefaces, jsf.

--- src/main/java/br/ufs/dcomp/locadora/entity/Veiculo.java
+++ src/main/java/br/ufs/dcomp/locadora/entity/Veiculo.java
@@ -1,5 +1,15 @@
 package br.ufs.dcomp.locadora.entity;
 
 public class Veiculo {
+	
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
 
 }
